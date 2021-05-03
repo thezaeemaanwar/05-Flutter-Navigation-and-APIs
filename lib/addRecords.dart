e7b622ce98aeb18fcf6beaabf62600342e53e57f
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-05-03 15:17:22 +0500

    🙌 INITIAL : Initial Commit

---
+++ lib/addRecords.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddRecords extends StatefulWidget {
+  AddRecords({Key key}) : super(key: key);
+
+  @override
+  _AddRecordsState createState() => _AddRecordsState();
+}
+
+class _AddRecordsState extends State<AddRecords> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
